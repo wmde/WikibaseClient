wikimedia/mediawiki-extensions-Wikibase

Make use of new sites code in core
Change-Id: Ie639247b59abce24c9c43420de8a642e63f0978b

client/tests/phpunit/includes/LangLinkHandlerTest.php
@@ -87,7 +87,7 @@ class LangLinkHandlerTest extends \MediaWikiTestCase {
 			'testwiki',
 			array( NS_MAIN ),
 			$this->mockRepo,
-			\Sites::singleton()
+			\SitesTable::newInstance()
 		);
 	}
 

wmde/WikibaseClient

tests/phpunit/includes/LangLinkHandlerTest.php
@@ -87,7 +87,7 @@ class LangLinkHandlerTest extends \MediaWikiTestCase {
 			'testwiki',
 			array( NS_MAIN ),
 			$this->mockRepo,
-			\Sites::singleton()
+			\SiteSQLStore::newInstance()
 		);
 	}
 