wikimedia/mediawiki-extensions-Wikibase

Introduce ChangeLineFormatter in the client
Change-Id: Ib03eb7395b0524cd27052b7e2305a85523170d7c

client/WikibaseClient.classes.php
@@ -44,9 +44,12 @@ return call_user_func( function() {
 		'Wikibase\PropertyParserFunctionRenderer' => 'includes/parserhooks/PropertyParserFunctionRenderer.php',
 
 		// includes/recentchanges
-		'Wikibase\ExternalChangesLine' => 'includes/recentchanges/ExternalChangesLine.php',
+		'Wikibase\ChangeLineFormatter' => 'includes/recentchanges/ChangeLineFormatter.php',
+		'Wikibase\ExternalChange' => 'includes/recentchanges/ExternalChange.php',
+		'Wikibase\ExternalChangeGenerator' => 'includes/recentchanges/ExternalChangeGenerator.php',
 		'Wikibase\ExternalRecentChange' => 'includes/recentchanges/ExternalRecentChange.php',
 		'Wikibase\RecentChangesFilterOptions' => 'includes/recentchanges/RecentChangesFilterOptions.php',
+		'Wikibase\RevisionObject' => 'includes/recentchanges/RevisionObject.php',
 
 		// includes/specials
 		'Wikibase\Client\Specials\SpecialUnconnectedPages' => 'includes/specials/SpecialUnconnectedPages.php',

wmde/WikibaseClient

WikibaseClient.classes.php
@@ -43,9 +43,12 @@ return call_user_func( function() {
 		'Wikibase\PropertyParserFunctionRenderer' => 'includes/parserhooks/PropertyParserFunctionRenderer.php',
 
 		// includes/recentchanges
-		'Wikibase\ExternalChangesLine' => 'includes/recentchanges/ExternalChangesLine.php',
+		'Wikibase\ChangeLineFormatter' => 'includes/recentchanges/ChangeLineFormatter.php',
+		'Wikibase\ExternalChange' => 'includes/recentchanges/ExternalChange.php',
+		'Wikibase\ExternalChangeFactory' => 'includes/recentchanges/ExternalChangeFactory.php',
 		'Wikibase\ExternalRecentChange' => 'includes/recentchanges/ExternalRecentChange.php',
 		'Wikibase\RecentChangesFilterOptions' => 'includes/recentchanges/RecentChangesFilterOptions.php',
+		'Wikibase\RevisionData' => 'includes/recentchanges/RevisionData.php',
 
 		// includes/specials
 		'Wikibase\Client\Specials\SpecialUnconnectedPages' => 'includes/specials/SpecialUnconnectedPages.php',