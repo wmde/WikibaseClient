wikimedia/mediawiki-extensions-Wikibase

Add {{WBREPONAME}} magic word
Change-Id: Ide94af740194788fff9f15b1878af961d5e0ce06

client/WikibaseClient.i18n.php
@@ -20,17 +20,17 @@ $messages = array();
  */
 $messages['en'] = array(
 	'wikibase-client-desc' => 'Client for the Wikibase extension',
-	'specialpages-group-wikibaseclient' => 'Wikidata client',
+	'specialpages-group-wikibaseclient' => '{{WBREPONAME}} client',
 	'tooltip-t-wikibase' => 'Link to connected data repository item',
 	'accesskey-t-wikibase' => 'g',
-	'wikibase-after-page-move' => 'You may also [$1 update] the associated Wikidata item to maintain language links on moved page.',
-	'wikibase-after-page-move-queued' => 'The [$1 Wikidata item] associated with this page will be automatically updated soon.',
-	'wikibase-comment-add' => 'A Wikidata item has been created.',
-	'wikibase-comment-remove' => 'Associated Wikidata item deleted. Language links removed.',
-	'wikibase-comment-linked' => 'A Wikidata item has been linked to this page.',
-	'wikibase-comment-unlink' => 'This page has been unlinked from Wikidata item. Language links removed.',
-	'wikibase-comment-restore' => 'Associated Wikidata item undeleted. Language links restored.',
-	'wikibase-comment-update' => 'Wikidata item changed',
+	'wikibase-after-page-move' => 'You may also [$1 update] the associated {{WBREPONAME}} item to maintain language links on moved page.',
+	'wikibase-after-page-move-queued' => 'The [$1 {{WBREPONAME}} item] associated with this page will be automatically updated soon.',
+	'wikibase-comment-add' => 'A {{WBREPONAME}} item has been created.',
+	'wikibase-comment-remove' => 'Associated {{WBREPONAME}} item deleted. Language links removed.',
+	'wikibase-comment-linked' => 'A {{WBREPONAME}} item has been linked to this page.',
+	'wikibase-comment-unlink' => 'This page has been unlinked from {{WBREPONAME}} item. Language links removed.',
+	'wikibase-comment-restore' => 'Associated {{WBREPONAME}} item undeleted. Language links restored.',
+	'wikibase-comment-update' => '{{WBREPONAME}} item changed',
 	'wikibase-comment-sitelink-add' => 'Language link added: $1',
 	'wikibase-comment-sitelink-change' => 'Language link changed from $1 to $2',
 	'wikibase-comment-sitelink-remove' => 'Language link removed: $1',
@@ -54,13 +54,13 @@ $messages['en'] = array(
 	'wikibase-linkitem-success-link' => 'The pages have successfully been linked. You can find the item containing the links in our [$1 central data repository].',
 	'wikibase-property-notfound' => '$1 property not found.',
 	'wikibase-property-notsupportedyet' => 'Wikibase does not yet support property type for $1 property.',
-	'wikibase-rc-hide-wikidata' => '$1 Wikidata',
-	'wikibase-rc-hide-wikidata-hide' => 'Hide',
-	'wikibase-rc-hide-wikidata-show' => 'Show',
-	'wikibase-rc-show-wikidata-pref' => 'Show Wikidata edits in recent changes',
+	'wikibase-rc-hide-wikibase' => '$1 {{WBREPONAME}}',
+	'wikibase-rc-hide-wikibase-hide' => 'Hide',
+	'wikibase-rc-hide-wikibase-show' => 'Show',
+	'wikibase-rc-show-wikibase-pref' => 'Show {{WBREPONAME}} edits in recent changes',
 	'wikibase-rc-wikibase-edit-letter' => 'D',
-	'wikibase-rc-wikibase-edit-title' => 'Wikidata edit',
-	'wikibase-watchlist-show-changes-pref' => 'Show Wikidata edits in your watchlist',
+	'wikibase-rc-wikibase-edit-title' => '{{WBREPONAME}} edit',
+	'wikibase-watchlist-show-changes-pref' => 'Show {{WBREPONAME}} edits in your watchlist',
 	'wikibase-error-serialize-error' => 'Failed to serialize data.',
 	'wikibase-error-invalid-entity-id' => 'The ID entered is unknown to the system. Please use a valid entity ID.',
 	'special-unconnectedpages' => 'Pages not connected to items',
@@ -71,7 +71,7 @@ $messages['en'] = array(
 	'wikibase-unconnectedpages-page-warning' => 'The page title could not be used for the query and is ignored.',
 	'wikibase-unconnectedpages-iwdata-label' => 'Only pages with interlanguage links',
 	'wikibase-unconnectedpages-format-row' => '($1 {{PLURAL:$1|interlanguage link|interlanguage links}} on the page)',
-	'wikibase-pageinfo-entity-id' => 'Wikidata Item ID',
+	'wikibase-pageinfo-entity-id' => '{{WBREPONAME}} Item ID',
 	'wikibase-pageinfo-entity-id-none' => 'None',
 	'wikibase-property-render-error' => 'Failed to render property $1: $2'
 );

wmde/WikibaseClient

Bug: 45852

WikibaseClient.i18n.php
@@ -20,17 +20,17 @@ $messages = array();
  */
 $messages['en'] = array(
 	'wikibase-client-desc' => 'Client for the Wikibase extension',
-	'specialpages-group-wikibaseclient' => 'Wikidata client',
+	'specialpages-group-wikibaseclient' => '{{WBREPONAME}} client',
 	'tooltip-t-wikibase' => 'Link to connected data repository item',
 	'accesskey-t-wikibase' => 'g',
-	'wikibase-after-page-move' => 'You may also [$1 update] the associated Wikidata item to maintain language links on moved page.',
-	'wikibase-after-page-move-queued' => 'The [$1 Wikidata item] associated with this page will be automatically updated soon.',
-	'wikibase-comment-add' => 'A Wikidata item has been created.',
-	'wikibase-comment-remove' => 'Associated Wikidata item deleted. Language links removed.',
-	'wikibase-comment-linked' => 'A Wikidata item has been linked to this page.',
-	'wikibase-comment-unlink' => 'This page has been unlinked from Wikidata item. Language links removed.',
-	'wikibase-comment-restore' => 'Associated Wikidata item undeleted. Language links restored.',
-	'wikibase-comment-update' => 'Wikidata item changed',
+	'wikibase-after-page-move' => 'You may also [$1 update] the associated {{WBREPONAME}} item to maintain language links on moved page.',
+	'wikibase-after-page-move-queued' => 'The [$1 {{WBREPONAME}} item] associated with this page will be automatically updated soon.',
+	'wikibase-comment-add' => 'A {{WBREPONAME}} item has been created.',
+	'wikibase-comment-remove' => 'Associated {{WBREPONAME}} item deleted. Language links removed.',
+	'wikibase-comment-linked' => 'A {{WBREPONAME}} item has been linked to this page.',
+	'wikibase-comment-unlink' => 'This page has been unlinked from {{WBREPONAME}} item. Language links removed.',
+	'wikibase-comment-restore' => 'Associated {{WBREPONAME}} item undeleted. Language links restored.',
+	'wikibase-comment-update' => '{{WBREPONAME}} item changed',
 	'wikibase-comment-sitelink-add' => 'Language link added: $1',
 	'wikibase-comment-sitelink-change' => 'Language link changed from $1 to $2',
 	'wikibase-comment-sitelink-remove' => 'Language link removed: $1',
@@ -54,13 +54,13 @@ $messages['en'] = array(
 	'wikibase-linkitem-success-link' => 'The pages have successfully been linked. You can find the item containing the links in our [$1 central data repository].',
 	'wikibase-property-notfound' => '$1 property not found.',
 	'wikibase-property-notsupportedyet' => 'Wikibase does not yet support property type for $1 property.',
-	'wikibase-rc-hide-wikidata' => '$1 Wikidata',
+	'wikibase-rc-hide-wikidata' => '$1 {{WBREPONAME}}',
 	'wikibase-rc-hide-wikidata-hide' => 'Hide',
 	'wikibase-rc-hide-wikidata-show' => 'Show',
-	'wikibase-rc-show-wikidata-pref' => 'Show Wikidata edits in recent changes',
+	'wikibase-rc-show-wikidata-pref' => 'Show {{WBREPONAME}} edits in recent changes',
 	'wikibase-rc-wikibase-edit-letter' => 'D',
-	'wikibase-rc-wikibase-edit-title' => 'Wikidata edit',
-	'wikibase-watchlist-show-changes-pref' => 'Show Wikidata edits in your watchlist',
+	'wikibase-rc-wikibase-edit-title' => '{{WBREPONAME}} edit',
+	'wikibase-watchlist-show-changes-pref' => 'Show {{WBREPONAME}} edits in your watchlist',
 	'wikibase-error-serialize-error' => 'Failed to serialize data.',
 	'wikibase-error-invalid-entity-id' => 'The ID entered is unknown to the system. Please use a valid entity ID.',
 	'special-unconnectedpages' => 'Pages not connected to items',
@@ -71,7 +71,7 @@ $messages['en'] = array(
 	'wikibase-unconnectedpages-page-warning' => 'The page title could not be used for the query and is ignored.',
 	'wikibase-unconnectedpages-iwdata-label' => 'Only pages with interlanguage links',
 	'wikibase-unconnectedpages-format-row' => '($1 {{PLURAL:$1|interlanguage link|interlanguage links}} on the page)',
-	'wikibase-pageinfo-entity-id' => 'Wikidata Item ID',
+	'wikibase-pageinfo-entity-id' => '{{WBREPONAME}} item ID',
 	'wikibase-pageinfo-entity-id-none' => 'None',
 	'wikibase-property-render-error' => 'Failed to render property $1: $2'
 );