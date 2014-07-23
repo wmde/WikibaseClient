wikimedia/mediawiki-extensions-Wikibase

Move more renderer construction code into RendererFactory
checking if variants are used and then which renderer to use
is more the responsibility of the RendererFactory.

Change-Id: I1857241f3b71308c32ba75ca6af49623c77e53cb

client/includes/DataAccess/PropertyParserFunction/RendererFactory.php
@@ -3,12 +3,11 @@
 namespace Wikibase\DataAccess\PropertyParserFunction;
 
 use Language;
+use Parser;
 use ValueFormatters\FormatterOptions;
 use Wikibase\LanguageFallbackChainFactory;
 use Wikibase\Lib\OutputFormatSnakFormatterFactory;
 use Wikibase\Lib\SnakFormatter;
-use Wikibase\Lib\Store\EntityLookup;
-use Wikibase\PropertyLabelResolver;
 
 /**
  * @since 0.5
@@ -49,6 +48,21 @@ class RendererFactory {
 	}
 
 	/**
+	 * @param Parser $parser
+	 *
+	 * @return Renderer
+	 */
+	public function newFromParser( Parser $parser ) {
+		if ( $this->useVariants( $parser ) ) {
+			$variants = $parser->getConverterLanguage()->getVariants();
+			return $this->newVariantsAwareRenderer( $variants );
+		} else {
+			$targetLanguage = $parser->getTargetLanguage();
+			return $this->newLanguageAwareRenderer( $targetLanguage );
+		}
+	}
+
+	/**
 	 * @param Language $language
 	 *
 	 * @return LanguageAwareRenderer
@@ -66,8 +80,31 @@ class RendererFactory {
 	 *
 	 * @return VariantsAwareRenderer
 	 */
-	public function newVariantsAwareRenderer( array $variants ) {
+	private function newVariantsAwareRenderer( array $variants ) {
 		return new VariantsAwareRenderer( $this, $variants );
+	}
+
+	/**
+	 * Check whether variants are used in this parser run.
+	 *
+	 * @param Parser $parser
+	 *
+	 * @return boolean
+	 */
+	private function isParserUsingVariants( Parser $parser ) {
+		$parserOptions = $parser->getOptions();
+		return $parser->OutputType() === Parser::OT_HTML && !$parserOptions->getInterfaceMessage()
+			&& !$parserOptions->getDisableContentConversion();
+	}
+
+	/**
+	 * @param Parser $parser
+	 *
+	 * @return boolean
+	 */
+	private function useVariants( Parser $parser ) {
+		$converterLanguageHasVariants = $parser->getConverterLanguage()->hasVariants();
+		return $this->isParserUsingVariants( $parser ) && $converterLanguageHasVariants;
 	}
 
 	/**

client/includes/DataAccess/PropertyParserFunction/Runner.php
@@ -2,23 +2,14 @@
 
 namespace Wikibase\DataAccess\PropertyParserFunction;
 
-use InvalidArgumentException;
-use Language;
 use Parser;
-use Status;
-use ValueFormatters\FormatterOptions;
 use Wikibase\Client\WikibaseClient;
 use Wikibase\DataModel\Entity\EntityId;
 use Wikibase\DataModel\SiteLink;
-use Wikibase\LanguageFallbackChainFactory;
-use Wikibase\Lib\PropertyLabelNotResolvedException;
-use Wikibase\Lib\SnakFormatter;
-use Wikibase\Lib\Store\EntityLookup;
 use Wikibase\Lib\Store\SiteLinkLookup;
-use Wikibase\PropertyLabelResolver;
 
 /**
- * Handler of the {{#property}} parser function.
+ * Runner for the {{#property}} parser function.
  *
  * @since 0.4
  *
@@ -69,85 +60,22 @@ class Runner {
 	public function runPropertyParserFunction( Parser $parser, $propertyLabelOrId ) {
 		wfProfileIn( __METHOD__ );
 
-		// @todo use id provided as argument, if arbitrary access allowed
-		$itemId = $this->getItemIdForConnectedPage( $parser );
+		// @todo use id provided as argument, if arbitrary access allowed,
+		// which means property ids might also be allowed here.
+		$entityId = $this->getItemIdForConnectedPage( $parser );
 
 		// @todo handle when site link is not there, such as site link / entity has been deleted...
-		if ( $itemId === null ) {
+		if ( $entityId === null ) {
 			wfProfileOut( __METHOD__ );
 			return '';
 		}
 
-		$rendered = $this->renderForEntityId( $parser, $itemId, $propertyLabelOrId );
+		$renderer = $this->rendererFactory->newFromParser( $parser );
+		$rendered = $renderer->render( $entityId, $propertyLabelOrId );
 		$result = $this->buildResult( $rendered );
 
 		wfProfileOut( __METHOD__ );
 		return $result;
-	}
-
-	/**
-	 * @param Parser $parser
-	 * @param EntityId $entityId
-	 * @param string $propertyLabelOrId
-	 *
-	 * @return string
-	 */
-	private function renderForEntityId( Parser $parser, EntityId $entityId, $propertyLabelOrId ) {
-		if ( $this->useVariants( $parser ) ) {
-			return $this->renderInVariants( $parser, $entityId, $propertyLabelOrId );
-		} else {
-			$targetLanguage = $parser->getTargetLanguage();
-			return $this->renderInLanguage( $entityId, $propertyLabelOrId, $targetLanguage );
-		}
-	}
-
-	/**
-	 * Check whether variants are used in this parser run.
-	 *
-	 * @param Parser $parser
-	 *
-	 * @return boolean
-	 */
-	private function isParserUsingVariants( Parser $parser ) {
-		$parserOptions = $parser->getOptions();
-		return $parser->OutputType() === Parser::OT_HTML && !$parserOptions->getInterfaceMessage()
-			&& !$parserOptions->getDisableContentConversion();
-	}
-
-	/**
-	 * @param Parser $parser
-	 *
-	 * @return boolean
-	 */
-	private function useVariants( Parser $parser ) {
-		$converterLanguageHasVariants = $parser->getConverterLanguage()->hasVariants();
-		return $this->isParserUsingVariants( $parser ) && $converterLanguageHasVariants;
-	}
-
-	/**
-	 * @param Parser $parser
-	 * @param EntityId $entityId
-	 * @param string $propertyLabelOrId
-	 *
-	 * @return string
-	 */
-	private function renderInVariants( Parser $parser, EntityId $entityId, $propertyLabelOrId ) {
-		$variants = $parser->getConverterLanguage()->getVariants();
-		$variantsRenderer = $this->rendererFactory->newVariantsAwareRenderer( $variants );
-
-		return $variantsRenderer->render( $entityId, $propertyLabelOrId );
-	}
-
-	/**
-	 * @param EntityId $entityId
-	 * @param string $propertyLabelOrId property label or ID (pXXX)
-	 * @param Language $language
-	 *
-	 * @return string
-	 */
-	private function renderInLanguage( EntityId $entityId, $propertyLabelOrId, Language $language ) {
-		$renderer = $this->rendererFactory->newLanguageAwareRenderer( $language );
-		return $renderer->render( $entityId, $propertyLabelOrId );
 	}
 
 	/**

client/tests/phpunit/includes/DataAccess/PropertyParserFunction/RendererFactoryTest.php
@@ -23,7 +23,48 @@ use Wikibase\Lib\SnakFormatter;
  * @licence GNU GPL v2+
  * @author Katie Filbert < aude.wiki@gmail.com >
  */
-class PropertyParserFunctionRendererFactoryTest extends \PHPUnit_Framework_TestCase {
+class RendererFactoryTest extends \PHPUnit_Framework_TestCase {
+
+	/**
+	 * @dataProvider newFromParserProvider
+	 */
+	public function testNewFromParser( $expected, $languageCode, $interfaceMessage,
+		$disableContentConversion, $disableTitleConversion, $outputType
+	) {
+		$parser = $this->getParser(
+			$languageCode,
+			$interfaceMessage,
+			$disableContentConversion,
+			$disableTitleConversion,
+			$outputType
+		);
+
+		$rendererFactory = new RendererFactory(
+			$this->getSnaksFinder(),
+			$this->getLanguageFallbackChainFactory(),
+			$this->getSnakFormatterFactory()
+		);
+
+		$renderer = $rendererFactory->newFromParser( $parser );
+
+		$this->assertInstanceOf( $expected, $renderer );
+	}
+
+	public function newFromParserProvider() {
+		$languageRendererClass = 'Wikibase\DataAccess\PropertyParserFunction\LanguageAwareRenderer';
+		$variantsRendererClass = 'Wikibase\DataAccess\PropertyParserFunction\VariantsAwareRenderer';
+
+		return array(
+			array( $languageRendererClass, 'en', false, false, false, Parser::OT_HTML ),
+			array( $languageRendererClass, 'ku', false, false, false, Parser::OT_PLAIN ),
+			array( $languageRendererClass, 'zh', false, false, false, Parser::OT_WIKI ),
+			array( $languageRendererClass, 'zh', false, true, false, Parser::OT_HTML ),
+			array( $languageRendererClass, 'zh', true, false, false, Parser::OT_HTML ),
+			array( $languageRendererClass, 'zh', false, false, false, Parser::OT_PREPROCESS ),
+			array( $variantsRendererClass, 'zh', false, false, true, Parser::OT_HTML ),
+			array( $variantsRendererClass, 'ku', false, false, false, Parser::OT_HTML ),
+		);
+	}
 
 	public function testNewLanguageAwareRenderer() {
 		$rendererFactory = new RendererFactory(
@@ -35,8 +76,8 @@ class PropertyParserFunctionRendererFactoryTest extends \PHPUnit_Framework_TestC
 		$language = Language::factory( 'he' );
 		$renderer = $rendererFactory->newLanguageAwareRenderer( $language );
 
-		$expectedClass = 'Wikibase\DataAccess\PropertyParserFunction\LanguageAwareRenderer';
-		$this->assertInstanceOf( $expectedClass, $renderer );
+		$languageRendererClass = 'Wikibase\DataAccess\PropertyParserFunction\LanguageAwareRenderer';
+		$this->assertInstanceOf( $languageRendererClass, $renderer );
 	}
 
 	private function getSnaksFinder() {
@@ -75,6 +116,40 @@ class PropertyParserFunctionRendererFactoryTest extends \PHPUnit_Framework_TestC
 			->will( $this->returnValue( $snakFormatter ) );
 
 		return $snakFormatterFactory;
+	}
+
+	private function getParser( $languageCode, $interfaceMessage, $disableContentConversion,
+		$disableTitleConversion, $outputType
+	) {
+		$parserConfig = array( 'class' => 'Parser' );
+
+		$parserOptions = $this->getParserOptions(
+			$languageCode,
+			$interfaceMessage,
+			$disableContentConversion,
+			$disableTitleConversion
+		);
+
+		$parser = new Parser( $parserConfig );
+
+		$parser->setTitle( Title::newFromText( 'Cat' ) );
+		$parser->startExternalParse( null, $parserOptions, $outputType );
+
+		return $parser;
+	}
+
+	private function getParserOptions( $languageCode, $interfaceMessage, $disableContentConversion,
+		$disableTitleConversion
+	) {
+		$language = Language::factory( $languageCode );
+
+		$parserOptions = new ParserOptions( User::newFromId( 0 ), $languageCode );
+		$parserOptions->setTargetLanguage( $language );
+		$parserOptions->setInterfaceMessage( $interfaceMessage );
+		$parserOptions->disableContentConversion( $disableContentConversion );
+		$parserOptions->disableTitleConversion( $disableTitleConversion );
+
+		return $parserOptions;
 	}
 
 }

client/tests/phpunit/includes/DataAccess/PropertyParserFunction/RunnerTest.php
@@ -30,54 +30,23 @@ use Wikibase\Test\MockRepository;
  */
 class RunnerTest extends \PHPUnit_Framework_TestCase {
 
-	/**
-	 * @return Runner
-	 */
-	private function getRunner() {
-		return new Runner(
+	public function testRunPropertyParserFunction() {
+		$runner = new Runner(
 			$this->getRendererFactory(),
 			$this->getSiteLinkLookup(),
 			'enwiki'
 		);
-	}
 
-	/**
-	 * @dataProvider runPropertyParserFunctionProvider
-	 */
-	public function testRunPropertyParserFunction(
-		$expectedRendered,
-		$languageCode,
-		$interfaceMessage,
-		$disableContentConversion,
-		$disableTitleConversion,
-		$outputType
-	) {
-		$parser = $this->getParser( $languageCode, $interfaceMessage, $disableContentConversion,
-			$disableTitleConversion, $outputType );
-
-		$runner = $this->getRunner();
-		$result = $runner->runPropertyParserFunction( $parser, 'gato' );
+		$parser = $this->getParser();
+		$result = $runner->runPropertyParserFunction( $parser, 'Cat' );
 
 		$expected = array(
-			$expectedRendered,
+			'meow!',
 			'noparse' => false,
 			'nowiki' => false
 		);
 
 		$this->assertEquals( $expected, $result );
-	}
-
-	public function runPropertyParserFunctionProvider() {
-		return array(
-			array( 'meow!', 'en', false, false, false, Parser::OT_HTML ),
-			array( 'meow!', 'ku', false, false, false, Parser::OT_PLAIN ),
-			array( 'meow!', 'zh', false, false, false, Parser::OT_WIKI ),
-			array( 'meow!', 'zh', false, true, false, Parser::OT_HTML ),
-			array( 'meow!', 'zh', true, false, false, Parser::OT_HTML ),
-			array( 'meow!', 'zh', false, false, false, Parser::OT_PREPROCESS ),
-			array( 'm30w!', 'zh', false, false, true, Parser::OT_HTML ),
-			array( 'm30w!', 'ku', false, false, false, Parser::OT_HTML )
-		);
 	}
 
 	private function getSiteLinkLookup() {
@@ -92,8 +61,7 @@ class RunnerTest extends \PHPUnit_Framework_TestCase {
 	}
 
 	private function getRendererFactory() {
-		$languageRenderer = $this->getLanguageAwareRenderer();
-		$variantsRenderer = $this->getVariantsAwareRenderer();
+		$renderer = $this->getRenderer();
 
 		$rendererFactory = $this->getMockBuilder(
 				'Wikibase\DataAccess\PropertyParserFunction\RendererFactory'
@@ -102,61 +70,31 @@ class RunnerTest extends \PHPUnit_Framework_TestCase {
 			->getMock();
 
 		$rendererFactory->expects( $this->any() )
-			->method( 'newLanguageAwareRenderer' )
-			->will( $this->returnValue( $languageRenderer ) );
-
-		$rendererFactory->expects( $this->any() )
-			->method( 'newVariantsAwareRenderer' )
-			->will( $this->returnValue( $variantsRenderer ) );
+			->method( 'newFromParser' )
+			->will( $this->returnValue( $renderer ) );
 
 		return $rendererFactory;
 	}
 
-	private function getLanguageAwareRenderer() {
-		$languageRenderer = $this->getMockBuilder(
-				'Wikibase\DataAccess\PropertyParserFunction\LanguageAwareRenderer'
+	private function getRenderer() {
+		$renderer = $this->getMockBuilder(
+				'Wikibase\DataAccess\PropertyParserFunction\Renderer'
 			)
 			->disableOriginalConstructor()
 			->getMock();
 
-		$languageRenderer->expects( $this->any() )
+		$renderer->expects( $this->any() )
 			->method( 'render' )
 			->will( $this->returnValue( 'meow!' ) );
 
-		return $languageRenderer;
+		return $renderer;
 	}
 
-	private function getVariantsAwareRenderer() {
-		$variantsRenderer = $this->getMockBuilder(
-				'Wikibase\DataAccess\PropertyParserFunction\VariantsAwareRenderer'
-			)
-			->disableOriginalConstructor()
-			->getMock();
-
-		$variantsRenderer->expects( $this->any() )
-			->method( 'render' )
-			->will( $this->returnValue( 'm30w!' ) );
-
-		return $variantsRenderer;
-	}
-
-	private function getParser( $languageCode, $interfaceMessage, $disableContentConversion,
-		$disableTitleConversion, $outputType
-	) {
+	private function getParser() {
 		$parserConfig = array( 'class' => 'Parser' );
 
 		$parser = new Parser( $parserConfig );
 		$parser->setTitle( Title::newFromText( 'Cat' ) );
-
-		$language = Language::factory( $languageCode );
-
-		$parserOptions = new ParserOptions( User::newFromId( 0 ), $languageCode );
-		$parserOptions->setTargetLanguage( $language );
-		$parserOptions->setInterfaceMessage( $interfaceMessage );
-		$parserOptions->disableContentConversion( $disableContentConversion );
-		$parserOptions->disableTitleConversion( $disableTitleConversion );
-
-		$parser->startExternalParse( null, $parserOptions, $outputType );
 
 		return $parser;
 	}

wmde/WikibaseClient

includes/DataAccess/PropertyParserFunction/RendererFactory.php
@@ -3,12 +3,11 @@
 namespace Wikibase\DataAccess\PropertyParserFunction;
 
 use Language;
+use Parser;
 use ValueFormatters\FormatterOptions;
 use Wikibase\LanguageFallbackChainFactory;
 use Wikibase\Lib\OutputFormatSnakFormatterFactory;
 use Wikibase\Lib\SnakFormatter;
-use Wikibase\Lib\Store\EntityLookup;
-use Wikibase\PropertyLabelResolver;
 
 /**
  * @since 0.5
@@ -49,6 +48,21 @@ class RendererFactory {
 	}
 
 	/**
+	 * @param Parser $parser
+	 *
+	 * @return Renderer
+	 */
+	public function newRendererFromParser( Parser $parser ) {
+		if ( $this->useVariants( $parser ) ) {
+			$variants = $parser->getConverterLanguage()->getVariants();
+			return $this->newVariantsAwareRenderer( $variants );
+		} else {
+			$targetLanguage = $parser->getTargetLanguage();
+			return $this->newLanguageAwareRenderer( $targetLanguage );
+		}
+	}
+
+	/**
 	 * @param Language $language
 	 *
 	 * @return LanguageAwareRenderer
@@ -66,8 +80,31 @@ class RendererFactory {
 	 *
 	 * @return VariantsAwareRenderer
 	 */
-	public function newVariantsAwareRenderer( array $variants ) {
+	private function newVariantsAwareRenderer( array $variants ) {
 		return new VariantsAwareRenderer( $this, $variants );
+	}
+
+	/**
+	 * Check whether variants are used in this parser run.
+	 *
+	 * @param Parser $parser
+	 *
+	 * @return boolean
+	 */
+	private function isParserUsingVariants( Parser $parser ) {
+		$parserOptions = $parser->getOptions();
+		return $parser->OutputType() === Parser::OT_HTML && !$parserOptions->getInterfaceMessage()
+			&& !$parserOptions->getDisableContentConversion();
+	}
+
+	/**
+	 * @param Parser $parser
+	 *
+	 * @return boolean
+	 */
+	private function useVariants( Parser $parser ) {
+		$converterLanguageHasVariants = $parser->getConverterLanguage()->hasVariants();
+		return $this->isParserUsingVariants( $parser ) && $converterLanguageHasVariants;
 	}
 
 	/**

includes/DataAccess/PropertyParserFunction/Runner.php
@@ -2,23 +2,14 @@
 
 namespace Wikibase\DataAccess\PropertyParserFunction;
 
-use InvalidArgumentException;
-use Language;
 use Parser;
-use Status;
-use ValueFormatters\FormatterOptions;
 use Wikibase\Client\WikibaseClient;
 use Wikibase\DataModel\Entity\EntityId;
 use Wikibase\DataModel\SiteLink;
-use Wikibase\LanguageFallbackChainFactory;
-use Wikibase\Lib\PropertyLabelNotResolvedException;
-use Wikibase\Lib\SnakFormatter;
-use Wikibase\Lib\Store\EntityLookup;
 use Wikibase\Lib\Store\SiteLinkLookup;
-use Wikibase\PropertyLabelResolver;
 
 /**
- * Handler of the {{#property}} parser function.
+ * Runner for the {{#property}} parser function.
  *
  * @since 0.4
  *
@@ -69,85 +60,22 @@ class Runner {
 	public function runPropertyParserFunction( Parser $parser, $propertyLabelOrId ) {
 		wfProfileIn( __METHOD__ );
 
-		// @todo use id provided as argument, if arbitrary access allowed
-		$itemId = $this->getItemIdForConnectedPage( $parser );
+		// @todo use id provided as argument, if arbitrary access allowed,
+		// which means property ids might also be allowed here.
+		$entityId = $this->getItemIdForConnectedPage( $parser );
 
 		// @todo handle when site link is not there, such as site link / entity has been deleted...
-		if ( $itemId === null ) {
+		if ( $entityId === null ) {
 			wfProfileOut( __METHOD__ );
 			return '';
 		}
 
-		$rendered = $this->renderForEntityId( $parser, $itemId, $propertyLabelOrId );
+		$renderer = $this->rendererFactory->newRendererFromParser( $parser );
+		$rendered = $renderer->render( $entityId, $propertyLabelOrId );
 		$result = $this->buildResult( $rendered );
 
 		wfProfileOut( __METHOD__ );
 		return $result;
-	}
-
-	/**
-	 * @param Parser $parser
-	 * @param EntityId $entityId
-	 * @param string $propertyLabelOrId
-	 *
-	 * @return string
-	 */
-	private function renderForEntityId( Parser $parser, EntityId $entityId, $propertyLabelOrId ) {
-		if ( $this->useVariants( $parser ) ) {
-			return $this->renderInVariants( $parser, $entityId, $propertyLabelOrId );
-		} else {
-			$targetLanguage = $parser->getTargetLanguage();
-			return $this->renderInLanguage( $entityId, $propertyLabelOrId, $targetLanguage );
-		}
-	}
-
-	/**
-	 * Check whether variants are used in this parser run.
-	 *
-	 * @param Parser $parser
-	 *
-	 * @return boolean
-	 */
-	private function isParserUsingVariants( Parser $parser ) {
-		$parserOptions = $parser->getOptions();
-		return $parser->OutputType() === Parser::OT_HTML && !$parserOptions->getInterfaceMessage()
-			&& !$parserOptions->getDisableContentConversion();
-	}
-
-	/**
-	 * @param Parser $parser
-	 *
-	 * @return boolean
-	 */
-	private function useVariants( Parser $parser ) {
-		$converterLanguageHasVariants = $parser->getConverterLanguage()->hasVariants();
-		return $this->isParserUsingVariants( $parser ) && $converterLanguageHasVariants;
-	}
-
-	/**
-	 * @param Parser $parser
-	 * @param EntityId $entityId
-	 * @param string $propertyLabelOrId
-	 *
-	 * @return string
-	 */
-	private function renderInVariants( Parser $parser, EntityId $entityId, $propertyLabelOrId ) {
-		$variants = $parser->getConverterLanguage()->getVariants();
-		$variantsRenderer = $this->rendererFactory->newVariantsAwareRenderer( $variants );
-
-		return $variantsRenderer->render( $entityId, $propertyLabelOrId );
-	}
-
-	/**
-	 * @param EntityId $entityId
-	 * @param string $propertyLabelOrId property label or ID (pXXX)
-	 * @param Language $language
-	 *
-	 * @return string
-	 */
-	private function renderInLanguage( EntityId $entityId, $propertyLabelOrId, Language $language ) {
-		$renderer = $this->rendererFactory->newLanguageAwareRenderer( $language );
-		return $renderer->render( $entityId, $propertyLabelOrId );
 	}
 
 	/**

tests/phpunit/includes/DataAccess/PropertyParserFunction/RendererFactoryTest.php
@@ -23,7 +23,48 @@ use Wikibase\Lib\SnakFormatter;
  * @licence GNU GPL v2+
  * @author Katie Filbert < aude.wiki@gmail.com >
  */
-class PropertyParserFunctionRendererFactoryTest extends \PHPUnit_Framework_TestCase {
+class RendererFactoryTest extends \PHPUnit_Framework_TestCase {
+
+	/**
+	 * @dataProvider newRendererFromParserProvider
+	 */
+	public function testNewRendererFromParser( $expected, $languageCode, $interfaceMessage,
+		$disableContentConversion, $disableTitleConversion, $outputType
+	) {
+		$parser = $this->getParser(
+			$languageCode,
+			$interfaceMessage,
+			$disableContentConversion,
+			$disableTitleConversion,
+			$outputType
+		);
+
+		$rendererFactory = new RendererFactory(
+			$this->getSnaksFinder(),
+			$this->getLanguageFallbackChainFactory(),
+			$this->getSnakFormatterFactory()
+		);
+
+		$renderer = $rendererFactory->newRendererFromParser( $parser );
+
+		$this->assertInstanceOf( $expected, $renderer );
+	}
+
+	public function newRendererFromParserProvider() {
+		$languageRendererClass = 'Wikibase\DataAccess\PropertyParserFunction\LanguageAwareRenderer';
+		$variantsRendererClass = 'Wikibase\DataAccess\PropertyParserFunction\VariantsAwareRenderer';
+
+		return array(
+			array( $languageRendererClass, 'en', false, false, false, Parser::OT_HTML ),
+			array( $languageRendererClass, 'ku', false, false, false, Parser::OT_PLAIN ),
+			array( $languageRendererClass, 'zh', false, false, false, Parser::OT_WIKI ),
+			array( $languageRendererClass, 'zh', false, true, false, Parser::OT_HTML ),
+			array( $languageRendererClass, 'zh', true, false, false, Parser::OT_HTML ),
+			array( $languageRendererClass, 'zh', false, false, false, Parser::OT_PREPROCESS ),
+			array( $variantsRendererClass, 'zh', false, false, true, Parser::OT_HTML ),
+			array( $variantsRendererClass, 'ku', false, false, false, Parser::OT_HTML ),
+		);
+	}
 
 	public function testNewLanguageAwareRenderer() {
 		$rendererFactory = new RendererFactory(
@@ -35,8 +76,8 @@ class PropertyParserFunctionRendererFactoryTest extends \PHPUnit_Framework_TestC
 		$language = Language::factory( 'he' );
 		$renderer = $rendererFactory->newLanguageAwareRenderer( $language );
 
-		$expectedClass = 'Wikibase\DataAccess\PropertyParserFunction\LanguageAwareRenderer';
-		$this->assertInstanceOf( $expectedClass, $renderer );
+		$languageRendererClass = 'Wikibase\DataAccess\PropertyParserFunction\LanguageAwareRenderer';
+		$this->assertInstanceOf( $languageRendererClass, $renderer );
 	}
 
 	private function getSnaksFinder() {
@@ -75,6 +116,40 @@ class PropertyParserFunctionRendererFactoryTest extends \PHPUnit_Framework_TestC
 			->will( $this->returnValue( $snakFormatter ) );
 
 		return $snakFormatterFactory;
+	}
+
+	private function getParser( $languageCode, $interfaceMessage, $disableContentConversion,
+		$disableTitleConversion, $outputType
+	) {
+		$parserConfig = array( 'class' => 'Parser' );
+
+		$parserOptions = $this->getParserOptions(
+			$languageCode,
+			$interfaceMessage,
+			$disableContentConversion,
+			$disableTitleConversion
+		);
+
+		$parser = new Parser( $parserConfig );
+
+		$parser->setTitle( Title::newFromText( 'Cat' ) );
+		$parser->startExternalParse( null, $parserOptions, $outputType );
+
+		return $parser;
+	}
+
+	private function getParserOptions( $languageCode, $interfaceMessage, $disableContentConversion,
+		$disableTitleConversion
+	) {
+		$language = Language::factory( $languageCode );
+
+		$parserOptions = new ParserOptions( User::newFromId( 0 ), $languageCode );
+		$parserOptions->setTargetLanguage( $language );
+		$parserOptions->setInterfaceMessage( $interfaceMessage );
+		$parserOptions->disableContentConversion( $disableContentConversion );
+		$parserOptions->disableTitleConversion( $disableTitleConversion );
+
+		return $parserOptions;
 	}
 
 }

tests/phpunit/includes/DataAccess/PropertyParserFunction/RunnerTest.php
@@ -30,54 +30,23 @@ use Wikibase\Test\MockRepository;
  */
 class RunnerTest extends \PHPUnit_Framework_TestCase {
 
-	/**
-	 * @return Runner
-	 */
-	private function getRunner() {
-		return new Runner(
+	public function testRunPropertyParserFunction() {
+		$runner = new Runner(
 			$this->getRendererFactory(),
 			$this->getSiteLinkLookup(),
 			'enwiki'
 		);
-	}
 
-	/**
-	 * @dataProvider runPropertyParserFunctionProvider
-	 */
-	public function testRunPropertyParserFunction(
-		$expectedRendered,
-		$languageCode,
-		$interfaceMessage,
-		$disableContentConversion,
-		$disableTitleConversion,
-		$outputType
-	) {
-		$parser = $this->getParser( $languageCode, $interfaceMessage, $disableContentConversion,
-			$disableTitleConversion, $outputType );
-
-		$runner = $this->getRunner();
-		$result = $runner->runPropertyParserFunction( $parser, 'gato' );
+		$parser = $this->getParser();
+		$result = $runner->runPropertyParserFunction( $parser, 'Cat' );
 
 		$expected = array(
-			$expectedRendered,
+			'meow!',
 			'noparse' => false,
 			'nowiki' => false
 		);
 
 		$this->assertEquals( $expected, $result );
-	}
-
-	public function runPropertyParserFunctionProvider() {
-		return array(
-			array( 'meow!', 'en', false, false, false, Parser::OT_HTML ),
-			array( 'meow!', 'ku', false, false, false, Parser::OT_PLAIN ),
-			array( 'meow!', 'zh', false, false, false, Parser::OT_WIKI ),
-			array( 'meow!', 'zh', false, true, false, Parser::OT_HTML ),
-			array( 'meow!', 'zh', true, false, false, Parser::OT_HTML ),
-			array( 'meow!', 'zh', false, false, false, Parser::OT_PREPROCESS ),
-			array( 'm30w!', 'zh', false, false, true, Parser::OT_HTML ),
-			array( 'm30w!', 'ku', false, false, false, Parser::OT_HTML )
-		);
 	}
 
 	private function getSiteLinkLookup() {
@@ -92,8 +61,7 @@ class RunnerTest extends \PHPUnit_Framework_TestCase {
 	}
 
 	private function getRendererFactory() {
-		$languageRenderer = $this->getLanguageAwareRenderer();
-		$variantsRenderer = $this->getVariantsAwareRenderer();
+		$renderer = $this->getRenderer();
 
 		$rendererFactory = $this->getMockBuilder(
 				'Wikibase\DataAccess\PropertyParserFunction\RendererFactory'
@@ -102,61 +70,31 @@ class RunnerTest extends \PHPUnit_Framework_TestCase {
 			->getMock();
 
 		$rendererFactory->expects( $this->any() )
-			->method( 'newLanguageAwareRenderer' )
-			->will( $this->returnValue( $languageRenderer ) );
-
-		$rendererFactory->expects( $this->any() )
-			->method( 'newVariantsAwareRenderer' )
-			->will( $this->returnValue( $variantsRenderer ) );
+			->method( 'newRendererFromParser' )
+			->will( $this->returnValue( $renderer ) );
 
 		return $rendererFactory;
 	}
 
-	private function getLanguageAwareRenderer() {
-		$languageRenderer = $this->getMockBuilder(
-				'Wikibase\DataAccess\PropertyParserFunction\LanguageAwareRenderer'
+	private function getRenderer() {
+		$renderer = $this->getMockBuilder(
+				'Wikibase\DataAccess\PropertyParserFunction\Renderer'
 			)
 			->disableOriginalConstructor()
 			->getMock();
 
-		$languageRenderer->expects( $this->any() )
+		$renderer->expects( $this->any() )
 			->method( 'render' )
 			->will( $this->returnValue( 'meow!' ) );
 
-		return $languageRenderer;
+		return $renderer;
 	}
 
-	private function getVariantsAwareRenderer() {
-		$variantsRenderer = $this->getMockBuilder(
-				'Wikibase\DataAccess\PropertyParserFunction\VariantsAwareRenderer'
-			)
-			->disableOriginalConstructor()
-			->getMock();
-
-		$variantsRenderer->expects( $this->any() )
-			->method( 'render' )
-			->will( $this->returnValue( 'm30w!' ) );
-
-		return $variantsRenderer;
-	}
-
-	private function getParser( $languageCode, $interfaceMessage, $disableContentConversion,
-		$disableTitleConversion, $outputType
-	) {
+	private function getParser() {
 		$parserConfig = array( 'class' => 'Parser' );
 
 		$parser = new Parser( $parserConfig );
 		$parser->setTitle( Title::newFromText( 'Cat' ) );
-
-		$language = Language::factory( $languageCode );
-
-		$parserOptions = new ParserOptions( User::newFromId( 0 ), $languageCode );
-		$parserOptions->setTargetLanguage( $language );
-		$parserOptions->setInterfaceMessage( $interfaceMessage );
-		$parserOptions->disableContentConversion( $disableContentConversion );
-		$parserOptions->disableTitleConversion( $disableTitleConversion );
-
-		$parser->startExternalParse( null, $parserOptions, $outputType );
 
 		return $parser;
 	}